3d3d903e3a93d1746f0c80845fe9bb8d52a4705e
Kevin-Kinyanjui <kevin.kinyanjui@student.moringaschool.com>
2023-03-17 21:11:50 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+function introduction(name) {
+    console.log(`Hi, my name is ${name}.`);
+}
+introduction("Aki");
+
+function introductionWithLanguage(name, language) {
+    console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
+introductionWithLanguage("Aki", "Ember.js");
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
+introductionWithLanguageOptional("Aki");
